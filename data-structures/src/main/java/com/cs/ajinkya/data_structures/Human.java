81cc7a4be532ed310865642cc010c50f6430b565
AJ <ajinkya.kulkarni77@gmail.com>
2018-09-21 20:05:26 -0700

    Created a class with its attributes

---
+++ data-structures/src/main/java/com/cs/ajinkya/data_structures/Human.java
@@ -0,0 +1,35 @@
+package com.cs.ajinkya.data_structures;
+
+public class Human {
+	
+	private String name;
+	private int age;
+	private int height;
+	private String eyeColor;
+	
+	public Human(String name, int age, int height, String eyeColor) {
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.eyeColor = eyeColor;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public String getEyeColor() {
+		return eyeColor;
+	}
+	
+	
+
+}
