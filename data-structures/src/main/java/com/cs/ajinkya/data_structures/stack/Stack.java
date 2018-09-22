042cfb1e10f070c62d11dabb76d8a2f96220cddb
AJ <ajinkya.kulkarni77@gmail.com>
2018-09-22 15:13:21 -0700

    Implemented boundry conditions for push and pop operations

--- data-structures/src/main/java/com/cs/ajinkya/data_structures/stack/Stack.java
+++ data-structures/src/main/java/com/cs/ajinkya/data_structures/stack/Stack.java
@@ -1,41 +1,52 @@
 package com.cs.ajinkya.data_structures.stack;
 
 public class Stack {
-	
+
 	private int maxSize;
 	private long[] stackArray;
 	private int top;
-	
+
 	public Stack(int size) {
 		this.maxSize = size;
 		this.stackArray = new long[size];
 		this.top = -1;
 	}
-	
+
 	public void push(int value) {
-		top++;
-		stackArray[top] = value;
+		if (isFull()) {
+			System.out.println("Stack is already full");
+		} else {
+			top++;
+			stackArray[top] = value;
+		}
+
 	}
-	
-	public long pop( ) {
-		int old_top = top;
-		top--;
-		return stackArray[old_top];
+
+	public long pop() {
+		if (isEmpty()) {
+			System.out.println("Stack is already empty");
+			return -1;
+		} else {
+			int old_top = top;
+			top--;
+			return stackArray[old_top];
+		}
+
 	}
-	
+
 	public long peak() {
-		if(top == -1) {
+		if (top == -1) {
 			return -1;
 		}
 		return stackArray[top];
 	}
-	
+
 	public boolean isEmpty() {
 		return (top == -1);
 	}
-	
+
 	public boolean isFull() {
-		return (top == (maxSize-1));
+		return (top == (maxSize - 1));
 	}
 
 }
