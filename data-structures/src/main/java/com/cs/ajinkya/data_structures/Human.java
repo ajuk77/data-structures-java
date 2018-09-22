cd72b0c1e1fede64e729b29be6e3666380e0d9b4
AJ <ajinkya.kulkarni77@gmail.com>
2018-09-21 20:12:10 -0700

    Added behavior to the class

--- data-structures/src/main/java/com/cs/ajinkya/data_structures/Human.java
+++ data-structures/src/main/java/com/cs/ajinkya/data_structures/Human.java
@@ -7,6 +7,10 @@ public class Human {
 	private int height;
 	private String eyeColor;
 	
+	public Human() {
+		
+	}
+	
 	public Human(String name, int age, int height, String eyeColor) {
 		this.name = name;
 		this.age = age;
@@ -30,6 +34,29 @@ public class Human {
 		return eyeColor;
 	}
 	
+	public void speak() {
+		System.out.println("Name: " + name);
+		System.out.println("Age: "+ age);
+		System.out.println("Height: "+ height);
+		System.out.println("EyeColor: "+ eyeColor);
+	}
+	
+	public void eat() {
+		System.out.println(name + " is eating..");
+	}
+	
+	public void walk() {
+		System.out.println(name + " is walking..");
+	}
+	
+	public void work() {
+		System.out.println(name + " is working..");
+	}
+	
+	@Override
+	public String toString() {
+		return "Name: " + name + ",Age:" + age + ",Height:" + height + ",EyeColor:"+ eyeColor;
+	}
 	
 
 }
