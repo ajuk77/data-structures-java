b42bd72b946e02539292cbec7d38a61381edfd88
AJ <ajinkya.kulkarni77@gmail.com>
2018-09-21 19:55:51 -0700

    First commit of my work

---
+++ data-structures/src/main/java/com/cs/ajinkya/data_structures/App.java
@@ -0,0 +1,13 @@
+package com.cs.ajinkya.data_structures;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is a project to learn data structures in Java" );
+    }
+}
