5090d9ce06f41c66aef89ed240a9e62f6e7f482e
AJ <ajinkya.kulkarni77@gmail.com>
2018-09-23 23:22:30 -0700

    Added logic to insert and delete the first element of the linkedlist

--- data-structures/src/main/java/com/cs/ajinkya/data_structures/linkedlist/App.java
+++ data-structures/src/main/java/com/cs/ajinkya/data_structures/linkedlist/App.java
@@ -3,8 +3,11 @@ package com.cs.ajinkya.data_structures.linkedlist;
 public class App {
 	
 	public static void main(String[] args) {
-		ListNode root = new ListNode(10);
-		System.out.println("Total elements of the list = "+ root.listLenght());
+		SinglyLinkedList linkedList = new SinglyLinkedList();
+		linkedList.insertAtBegin(100);
+		linkedList.insertAtBegin(50);
+		
+		linkedList.displayList();
 	}
 
 }
---
+++ data-structures/src/main/java/com/cs/ajinkya/data_structures/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,41 @@
+package com.cs.ajinkya.data_structures.linkedlist;
+
+public class SinglyLinkedList {
+	
+	ListNode head;
+	private int length;
+	
+	public SinglyLinkedList() {
+		length = 0;
+	}
+	
+	public boolean isEmpty() {
+		return (null == head);
+	}
+	
+	public void insertAtBegin(int data) {
+		ListNode node = new ListNode(data);
+		node.setNext(head);
+		head = node;
+		length++;
+	}
+	
+	public ListNode deleteFirst() {
+		if(null == head) {
+			return null;
+		}
+		ListNode tempNode = head;
+		head = head.getNext();
+		tempNode.setNext(null);
+		return head;
+	}
+	
+	public void displayList() {
+		ListNode temp = head;
+		while(temp!=null) {
+			System.out.print(temp.getData() + " -> ");
+			temp = temp.getNext();
+		}
+	}
+
+}
