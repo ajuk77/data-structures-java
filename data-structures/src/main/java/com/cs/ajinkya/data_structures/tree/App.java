03ed7ae0ac50012d44032b639795108bd24e1ef3
ajinkya.kulkarni77@gmail.com <ajinkya.kulkarni77@gmail.com>
2018-11-26 18:53:38 -0800

    Added implemenration to create binary tree using bfs

--- data-structures/src/main/java/com/cs/ajinkya/data_structures/tree/App.java
+++ data-structures/src/main/java/com/cs/ajinkya/data_structures/tree/App.java
@@ -1,13 +1,67 @@
 package com.cs.ajinkya.data_structures.tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class App {
-	
+
 	public static void main(String[] args) {
-		
-		
+
 	}
-	
+
 	public static BinaryTreeNode getTree() {
+		List<Integer> treeNodes = new ArrayList<Integer>();
+		treeNodes.add(1);
+		treeNodes.add(2);
+		treeNodes.add(3);
+		treeNodes.add(4);
+		treeNodes.add(5);
+		treeNodes.add(6);
+		treeNodes.add(7);
+
+		BinaryTreeNode root = null;
+		for (int data : treeNodes) {
+			root = createBinaryTree(root, data);
+		}
+		return root;
+
+	}
+
+	public static BinaryTreeNode createBinaryTree(BinaryTreeNode root, int data) {
+		if (root == null) {
+			root = new BinaryTreeNode(data);
+			return root;
+		}
+		Queue<BinaryTreeNode> queue = new LinkedList<BinaryTreeNode>();
+		queue.offer(root);
+		BinaryTreeNode temp = null;
+		while (!queue.isEmpty()) {
+			temp = queue.poll();
+			if (temp != null) {
+				if (temp.left != null) {
+					queue.offer(temp.left);
+				} else {
+					temp.left = new BinaryTreeNode(data);
+					return root;
+				}
+				if (temp.right != null) {
+					queue.offer(temp.right);
+				} else {
+					temp.right = new BinaryTreeNode(data);
+					return root;
+				}
+
+			}
+
+		}
+		return root;
+
+	}
+
+	public static BinaryTreeNode getStaticTree() {
+
 		BinaryTreeNode root = null;
 		root = new BinaryTreeNode(1);
 		BinaryTreeNode node2 = new BinaryTreeNode(2);
@@ -23,6 +77,7 @@ public class App {
 		BinaryTreeNode node7 = new BinaryTreeNode(7);
 		node3.right = node7;
 		return root;
+
 	}
 
 }
